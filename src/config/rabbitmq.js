bd85221e0e024392818eeec3aa5448154163be3d
David <davidfamoyegun1@gmail.com>
2025-07-14 19:55:26 +0100

    Update to improve setup of project

--- src/config/rabbitmq.js
+++ src/config/rabbitmq.js
@@ -6,9 +6,16 @@ const QUEUE_NAME = process.env.QUEUE_NAME;
 
 let channel;
 
-const conenctRabbitMQ = async () => {
+const connectRabbitMQ = async () => {
     try {
-        const connection = await amqp.connect(RABBITMQ_URL);
+        const connection = await amqp.connect({
+            protocol: 'amqp',
+            hostname: process.env.RABBITMQ_HOST, // e.g. 'localhost' or 'rabbitmq'
+            port: 5672,
+            username: process.env.RABBITMQ_USER,
+            password: process.env.RABBITMQ_PASS,
+            frameMax: 8192,
+        });
         channel = await connection.createChannel();
         await channel.assertQueue(QUEUE_NAME, { durable : true });
         console.log(`Connected to RabbitMQ, listening on queue: ${QUEUE_NAME}`);
@@ -19,4 +26,4 @@ const conenctRabbitMQ = async () => {
 
 const getChannel = () => channel;
 
-module.exports = { conenctRabbitMQ, getChannel };
+module.exports = { connectRabbitMQ, getChannel };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { conenctRabbitMQ } = require("./config/rabbitmq");
+const { conenctRabbitMQ, connectRabbitMQ} = require("./config/rabbitmq");
 const consumeMessages = require("./consumers/rabbitConsumer");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const startServer = async () => {
-    await conenctRabbitMQ();
-    consumeMessages();
+    await connectRabbitMQ();
+    await consumeMessages();
 
     app.listen(PORT, () => {
         console.log(`Email service runnig on port ${PORT}`);
